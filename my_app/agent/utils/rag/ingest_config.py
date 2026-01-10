33f5b3521e2622ab305fe4741356456cbadc0171
mukkss <muksss1102@gmail.com>
2026-01-11 03:11:41 +0530

    feat: added schema definition to ingest configuration

--- my_app/agent/utils/rag/ingest_config.py
+++ my_app/agent/utils/rag/ingest_config.py
@@ -5,18 +5,21 @@ BASE_DIR = Path(__file__).resolve().parents[3]
 
 INGEST_CONFIG = {
     "policy": {
+        "schema": "public",
         "table": "policy_table",
         "path": str(BASE_DIR / "data" / "Policies"),
         "chunk_size": 1000,
         "overlap": 200
     },
     "faq": {
+        "schema": "public",
         "table": "faq_table",
         "path": str(BASE_DIR / "data" / "FAQ"),
         "chunk_size": 1000,
         "overlap": 200
     },
     "guide": {
+        "schema": "public",
         "table": "guide_table",
         "path": str(BASE_DIR / "data" / "Guides"),
         "chunk_size": 1000,
